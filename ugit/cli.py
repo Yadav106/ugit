c10f6e03368b1def2b56442d9ac525ac2aa3a6ea
Yadav106 <sahil.jp.yadav@gmail.com>
2024-11-22 16:46:41 +0530

    added hashing and getting data from hashed objects

--- ugit/cli.py
+++ ugit/cli.py
@@ -1,8 +1,7 @@
 import argparse
-import os
+import sys
 
 from ugit import data
-from ugit.data import GIT_DIR
 
 def main():
     args = parse_args()
@@ -17,9 +16,24 @@ def parse_args():
     init_parser = commands.add_parser("init", description="initializes ugit")
     init_parser.set_defaults(func=init)
 
+    hash_object_parser = commands.add_parser("hash-object")
+    hash_object_parser.set_defaults(func=hash_object)
+    hash_object_parser.add_argument("file")
+
+    cat_file_parser = commands.add_parser("cat-file")
+    cat_file_parser.set_defaults(func=cat_file)
+    cat_file_parser.add_argument("object")
+
     return parser.parse_args()
     
 
 def init(args):
     print(data.init())
 
+def hash_object(args):
+    with open(args.file, 'rb') as f:
+        print(data.hash_object(f.read()))
+
+def cat_file(args):
+    sys.stdout.flush()
+    sys.stdout.buffer.write(data.get_object(args.object))
--- ugit/data.py
+++ ugit/data.py
@@ -1,4 +1,5 @@
 import os
+import hashlib
 
 GIT_DIR = ".ugit"
 
@@ -14,6 +15,35 @@ def init():
     """
     try:
         os.makedirs(GIT_DIR)
+        os.makedirs(os.path.join(GIT_DIR, 'objects'))
         return f"Initialized empty ugit repository in {os.path.join(os.getcwd(), GIT_DIR)}"
     except FileExistsError:
         return f"A ugit repository already exists in {os.getcwd()}"
+
+def hash_object(data):
+    """
+    Hashes a given file and stores it in .ugit/objects/
+
+    Args:
+        data - str: the contents of the file to be hashed
+
+    Returns:
+        oid - str: the object id of the hashed file
+    """
+    oid = hashlib.sha1(data).hexdigest()
+    with open(f"{GIT_DIR}/objects/{oid}", "wb") as out:
+        out.write(data)
+    return oid
+
+def get_object(oid):
+    """
+    Returns the contents of a hashed object
+
+    Args:
+        oid - str: the object id of the hashed file
+
+    Returns:
+        str: the contents of the hashed file
+    """
+    with open(f"{GIT_DIR}/objects/{oid}", "rb") as f:
+        return f.read()
