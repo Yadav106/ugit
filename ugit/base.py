a3f33162212af8d6372d6922a3bf73b59a9be986
Yadav106 <sahil.jp.yadav@gmail.com>
2024-11-24 11:15:02 +0530

    finished working on write-tree

--- ugit/base.py
+++ ugit/base.py
@@ -1,7 +1,9 @@
 import os
 from ugit import data
 
-def write_tree(directory="."):
+def write_tree(directory=".") -> str:
+    entries = []
+
     with os.scandir(directory) as it:
         for entry in it:
             full = os.path.join(directory, entry.name)
@@ -9,9 +11,23 @@ def write_tree(directory="."):
                 continue
 
             if entry.is_file(follow_symlinks=False):
-                print(full)
+                type_ = "blob"
+                with open(full, 'rb') as f:
+                    oid = data.hash_object(f.read())
             elif entry.is_dir(follow_symlinks=False):
-                write_tree(full)
+                type_ = "tree"
+                oid = write_tree(full)
+            else: # initialize type_ and oid with empty valyes
+                type_ = ""
+                oid = ""
+
+            entries.append((entry.name, oid, type_))
+
+    tree = "".join(f"{type_} {oid} {name}\n"
+                   for name, oid, type_
+                   in sorted(entries))
+
+    return data.hash_object(tree.encode(), "tree")
 
 
 def is_ignored(path:str):
